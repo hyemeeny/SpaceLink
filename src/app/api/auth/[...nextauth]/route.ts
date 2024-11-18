06382d692c738e91788b7d08bd608bb36b244000
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-18 21:05:44 +0900

    [Feat] NextAuth api 추가

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "이메일",
+          type: "text",
+          placeholder: "이메일 주소를 입력해 주세요.",
+        },
+        password: { label: "비밀번호", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/auth/sign-in`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              email: credentials?.email,
+              password: credentials?.password,
+            }),
+          }
+        );
+        const user = await res.json();
+        console.log("$$$user: ", user);
+
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
+  ],
+});
+
+export { handler as GET, handler as POST };
