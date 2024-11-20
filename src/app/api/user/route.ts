391d273fd3e16b8dd04946feae87ff77558b7e30
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-20 20:45:43 +0900

    [Chore] 유저 api 수정

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -2,25 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import { axiosInstance } from "@/lib/axiosInstance";
 import { cookies } from "next/headers";
 
-export const GET = async (req: NextRequest) => {
+export const GET = async () => {
   try {
-    // 쿠키에서 accessToken 가져오기
-    const accessToken = cookies().get("accessToken");
+    const accessToken = cookies().get("accessToken")?.value;
 
     if (!accessToken) {
       return NextResponse.json({ error: "인증 에러" }, { status: 401 });
     }
 
-    // 액세스 토큰을 사용하여 백엔드에서 유저 정보 요청
     const response = await axiosInstance.get("/users", {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
 
-    // 유저 정보 반환
     return NextResponse.json(response.data, { status: 201 });
   } catch (error) {
-    return NextResponse.json({ error: "유저 조회 오류" }, { status: 500 });
+    console.error("유저 조회 오류:", error);
+    return NextResponse.json({ error: "유저 조회 오류" }, { status: 401 });
   }
 };
