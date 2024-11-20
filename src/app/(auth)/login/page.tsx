c5654de1fdf22be16c36c1e31ff3cc20a2c3675f
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-20 20:44:08 +0900

    [Chore] 로그인 페이지 수정

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -6,21 +6,18 @@ import { useForm } from "react-hook-form";
 
 import Link from "next/link";
 import Image from "next/image";
-import Button from "@/components/Button";
+import Button from "@/components/Button/CtaButton";
 import Input from "@/components/Input";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+import { useAuth } from "@/hooks/useAuth";
 
 // 비밀번호 조건 정규표현식
-const passwordRegex =
-  /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$&*?!%])[A-Za-z\d!@$%&*?]{8,15}$/;
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$&*?!%])[A-Za-z\d!@$%&*?]{8,15}$/;
 
 // 로그인 스키마 정의
 const LoginSchema = z.object({
-  email: z
-    .string()
-    .min(1, { message: "이메일을 입력해주세요." })
-    .email({ message: "유효한 이메일을 입력해주세요." }),
+  email: z.string().min(1, { message: "이메일을 입력해주세요." }).email({ message: "유효한 이메일을 입력해주세요." }),
   password: z
     .string()
     .min(8, { message: "비밀번호를 8자리 이상 입력해 주세요." })
@@ -34,6 +31,7 @@ type LoginFormValues = z.infer<typeof LoginSchema>;
 
 const LoginPage = () => {
   const router = useRouter();
+  const { login, isPending, isError } = useAuth();
 
   const {
     register,
@@ -50,9 +48,10 @@ const LoginPage = () => {
 
   const onSubmit = async (data: LoginFormValues) => {
     try {
-      console.log(data);
-      const response = await axios.post("/api/auth/sign-in", data);
-      console.log("로그인 성공", response);
+      // console.log(data);
+      // const response = await axios.post("/api/auth/sign-in", data);
+      // console.log("로그인 성공", response);
+      await login(data);
       router.push("/");
     } catch (error) {
       console.error("로그인에 실패하였습니다.", error);
@@ -69,24 +68,14 @@ const LoginPage = () => {
         </h1>
         <p className="text-black text-base">
           회원이 아니신가요?
-          <Link
-            href={"/signup"}
-            className="text-purple01 font-semibold border-b-[1px] border-purple01 ml-3"
-          >
+          <Link href={"/signup"} className="text-purple01 font-semibold border-b-[1px] border-purple01 ml-3">
             회원 가입하기
           </Link>
         </p>
       </div>
 
       <form className="grid gap-6" onSubmit={handleSubmit(onSubmit)}>
-        <Input
-          label="이메일"
-          id="email"
-          type="email"
-          placeholder="test@test.com"
-          errors={errors.email?.message}
-          {...register("email")}
-        />
+        <Input label="이메일" id="email" type="email" placeholder="test@test.com" errors={errors.email?.message} {...register("email")} />
         <Input
           label="비밀번호"
           id="password"
@@ -105,12 +94,7 @@ const LoginPage = () => {
         <p className="text-gray05 text-sm">소설 로그인</p>
         <div className="flex gap-4">
           <Link href="#">
-            <Image
-              src="/icons/google.svg"
-              width={42}
-              height={42}
-              alt="Google"
-            />
+            <Image src="/icons/google.svg" width={42} height={42} alt="Google" />
           </Link>
           <Link href="#">
             <Image src="/icons/kakao.svg" width={42} height={42} alt="Kakao" />
