841ea5a4cfe1b4e47cb0b444c7329e3a08bdab84
JIHYEMIN <hyemeeeny@gmail.com>
2024-11-21 21:26:50 +0900

    [Chore] 로그아웃 api 함수 수정

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export const GET = async () => {
   try {
     const response = NextResponse.json({ message: "로그아웃 성공" });
     response.cookies.set("accessToken", "", { maxAge: 0, path: "/" });
@@ -9,4 +9,4 @@ export async function GET() {
     console.error("로그아웃 중 오류 발생:", error);
     return NextResponse.json({ error: "로그아웃 실패" }, { status: 500 });
   }
-}
+};
