723495a661c8ca75b637d26782884ccd32f20602
JIHYEMIN <hyemeeeny@gmail.com>
2025-01-06 18:33:21 +0900

    Chore: 배경 컬러 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="bg-gray01">
+      <body>
         <QueryClientProvider>
           <Header />
           {children}
