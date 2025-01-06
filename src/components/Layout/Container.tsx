8450fb088b5ca92522fb154986d910e6e8803e34
JIHYEMIN <hyemeeeny@gmail.com>
2025-01-06 18:33:52 +0900

    [Chore] 컨테이너 수정

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -1,9 +1,7 @@
 import { ReactNode } from "react";
 
 const Container = ({ children }: { children: ReactNode }) => {
-  return (
-    <section className="w-full lg:max-w-[1060px] mx-auto">{children}</section>
-  );
+  return <section className="w-full mx-auto px-4 box-border">{children}</section>;
 };
 
 export default Container;
