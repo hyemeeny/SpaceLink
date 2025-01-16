2890564fba37e5fcd155db507cc1c09956688948
JIHYEMIN <hyemeeeny@gmail.com>
2025-01-16 16:54:15 +0900

    [Feat] links 서버 액션 추가

--- src/actions/folders.ts
+++ src/actions/folders.ts
@@ -3,7 +3,6 @@
 import { revalidateTag } from "next/cache";
 import { cookies } from "next/headers";
 import API_URL from "@/constants/config";
-import { Link, LinkProps } from "@/types/links";
 
 // 폴더 생성
 export const postFolders = async (formData: FormData) => {
@@ -62,31 +61,5 @@ export const deleteFolders = async (folderId: number) => {
     if (error instanceof Error) {
       return { status: 500, message: error.message };
     }
-  }
-};
-
-export const getLinksById = async (folderId: number): Promise<LinkProps> => {
-  const accessToken = cookies().get("accessToken")?.value;
-
-  if (!accessToken) {
-    throw new Error("Access token is missing.");
-  }
-
-  try {
-    const response = await fetch(`${API_URL}/folders/${folderId}/links`, {
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${accessToken}`,
-      },
-      next: { tags: ["links"] },
-    });
-
-    if (!response.ok) {
-      console.error("링크를 조회하는 데 실패했습니다.");
-    }
-
-    return response.json();
-  } catch (error) {
-    throw new Error("링크 조회 중 에러 발생");
   }
 };
