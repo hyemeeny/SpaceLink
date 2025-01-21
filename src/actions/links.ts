ae608d2ba77219bb6caa6591d18e9707e4f15188
JIHYEMIN <hyemeeeny@gmail.com>
2025-01-21 21:15:46 +0900

    [Feat] 링크 생성 api 추가

--- src/actions/links.ts
+++ src/actions/links.ts
@@ -5,6 +5,7 @@ import { cookies } from "next/headers";
 import API_URL from "@/constants/config";
 import { LinkProps } from "@/types/links";
 
+// 링크 조회
 export const getLinksById = async (folderId: number): Promise<LinkProps> => {
   const accessToken = cookies().get("accessToken")?.value;
 
@@ -28,5 +29,32 @@ export const getLinksById = async (folderId: number): Promise<LinkProps> => {
     return response.json();
   } catch (error) {
     throw new Error("링크 조회 중 에러 발생");
+  }
+};
+
+// 링크 생성
+export const postLinks = async (data: { url: string; folderId: number }) => {
+  const accessToken = cookies().get("accessToken")?.value;
+  // const data = Object.fromEntries(formData.entries()); // Object.fromEntries는 키값 쌍의 목록을 객체로 바꿔준다
+
+  if (!accessToken) {
+    throw new Error("Access token is missing.");
+  }
+
+  try {
+    const response = await fetch(`${API_URL}/links`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify(data),
+    });
+
+    console.log("링크 등록", data);
+
+    revalidateTag("links");
+  } catch (error) {
+    console.error(error);
   }
 };
