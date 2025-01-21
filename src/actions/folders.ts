1e52d6d835b85c8fdcc694b25d77b251d3c06e1e
JIHYEMIN <hyemeeeny@gmail.com>
2025-01-21 21:17:07 +0900

    [Feat] 폴더 수정 api 추가

--- src/actions/folders.ts
+++ src/actions/folders.ts
@@ -5,9 +5,37 @@ import { cookies } from "next/headers";
 import API_URL from "@/constants/config";
 
 // 폴더 생성
-export const postFolders = async (formData: FormData) => {
+// export const postFolders = async (formData: FormData) => {
+//   const accessToken = cookies().get("accessToken")?.value;
+//   const data = Object.fromEntries(formData.entries());
+
+//   if (!accessToken) {
+//     throw new Error("인증 정보가 유효하지 않습니다.");
+//   }
+
+//   const response = await fetch(`${API_URL}/folders`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${accessToken}`,
+//     },
+//     body: JSON.stringify(data),
+//   });
+
+//   // if (!response.ok) {
+//   //   const errorResponse = await response.json();
+//   //   return {
+//   //     success: false,
+//   //     message: errorResponse?.message || "폴더 생성 실패",
+//   //   };
+//   // }
+
+//   revalidateTag("folders");
+//   //return { name, success: true, message: "폴더 생성 완료", isError: false };
+// };
+
+export const postFolders = async ({ name }: { name: string }) => {
   const accessToken = cookies().get("accessToken")?.value;
-  const data = Object.fromEntries(formData.entries());
 
   if (!accessToken) {
     throw new Error("인증 정보가 유효하지 않습니다.");
@@ -19,19 +47,48 @@ export const postFolders = async (formData: FormData) => {
       "Content-Type": "application/json",
       Authorization: `Bearer ${accessToken}`,
     },
-    body: JSON.stringify(data),
+    body: JSON.stringify({ name }),
   });
 
-  // if (!response.ok) {
-  //   const errorResponse = await response.json();
-  //   return {
-  //     success: false,
-  //     message: errorResponse?.message || "폴더 생성 실패",
-  //   };
-  // }
+  if (!response.ok) {
+    const errorResponse = await response.json();
+    return { status: response.status, message: errorResponse.message };
+  }
 
   revalidateTag("folders");
   //return { name, success: true, message: "폴더 생성 완료", isError: false };
+};
+
+// 폴더 수정
+export const putFolders = async ({ name, folderId }: { name: string; folderId: number }) => {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  try {
+    if (!accessToken) {
+      throw new Error("인증 정보가 유효하지 않습니다.");
+    }
+
+    const response = await fetch(`${API_URL}/folders/${folderId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ name }),
+    });
+
+    if (!response.ok) {
+      const errorResponse = await response.json();
+      return { status: response.status, message: errorResponse.message };
+    }
+
+    revalidateTag("folders");
+    return { status: 201, message: "폴더 수정 완료" };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { status: 500, message: error.message };
+    }
+  }
 };
 
 // 폴더 삭제
@@ -46,6 +103,7 @@ export const deleteFolders = async (folderId: number) => {
     const response = await fetch(`${API_URL}/folders/${folderId}`, {
       method: "DELETE",
       headers: {
+        "Content-Type": "application/json",
         Authorization: `Bearer ${accessToken}`,
       },
     });
