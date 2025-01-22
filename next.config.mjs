efd5396ea68a96d49c756f54576da869516dbf63
JIHYEMIN <hyemeeeny@gmail.com>
2025-01-22 20:43:26 +0900

    [Build] 빌드 오류 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,20 +3,28 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     remotePatterns: [
+      // {
+      //   protocol: "https",
+      //   hostname: "codeit-assets.codeit.com",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "s.pstatic.net",
+      //   pathname: "/**",
+      // },
+      // {
+      //   protocol: "https",
+      //   hostname: "images.velog.io",
+      //   pathname: "/**",
+      // },
       {
-        protocol: "https",
-        hostname: "codeit-assets.codeit.com",
-        pathname: "/**",
+        protocol: "http",
+        hostname: "**",
       },
       {
         protocol: "https",
-        hostname: "s.pstatic.net",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.velog.io",
-        pathname: "/**",
+        hostname: "**",
       },
     ],
   },
