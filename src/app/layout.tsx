a74db723f4e590646fc3a159e2eee5b8745c5830
JIHYEMIN <hyemeeeny@gmail.com>
2025-02-10 19:01:36 +0900

    [Delete] 리액트 쿼리 및 라우터 핸들러 api 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,21 @@
 import type { Metadata } from "next";
-import "../styles/globals.css";
-import { Providers as QueryClientProvider } from "./providers";
+import localFont from "next/font/local";
+import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
-import Header from "@/components/Layout/Header";
+import Header from "@/components/Layout/header/Header";
+import ClientPathname from "@/components/ClientPathname";
 
 export const metadata: Metadata = {
   title: "Linkbrary",
   description: "나만의 링크를 관리하는 Linkbrary",
 };
+
+const pretendard = localFont({
+  src: "../fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
 
 const ToastProvider = () => {
   return <Toaster />;
@@ -19,13 +27,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="kr" className={`${pretendard.variable}`}>
       <body>
-        <QueryClientProvider>
-          <ToastProvider />
+        <ToastProvider />
+        <ClientPathname hideRoutes={["/login", "signup"]}>
           <Header />
-          {children}
-        </QueryClientProvider>
+        </ClientPathname>
+        {children}
         <div id="modal-root"></div>
       </body>
     </html>
