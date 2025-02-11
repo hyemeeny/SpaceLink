8187f013dd6a9e209c22c4d8c98c3041f5890f12
JIHYEMIN <hyemeeeny@gmail.com>
2025-02-11 20:32:29 +0900

    [Feat] 링크 수정, 삭제 api 추가

--- src/actions/links.ts
+++ src/actions/links.ts
@@ -61,3 +61,55 @@ export const postLinks = async (linkData: { url: string; folderId: number }) => 
     console.error("링크 생성 중 에러 발생", error);
   }
 };
+
+// 링크 수정
+export const putLinks = async ({ url, linkId }: { url: string; linkId: number }) => {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  if (!accessToken) {
+    throw new Error("인증 정보가 유효하지 않습니다.");
+  }
+
+  try {
+    const response = await fetch(`${API_URL}/links/${linkId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ url }),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+
+    revalidateTag("links");
+  } catch (error) {
+    console.error("링크 수정 중 오류 발생", error);
+  }
+};
+
+// 링크 삭제
+export const deleteLinks = async (linkId: number) => {
+  const accessToken = cookies().get("accessToken")?.value;
+
+  try {
+    const response = await fetch(`${API_URL}/links/${linkId}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message);
+    }
+
+    revalidateTag("links");
+  } catch (error) {
+    console.error("링크 삭제 중 에러 발생", error);
+  }
+};
