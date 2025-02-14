511330df1c5172809f17a823a5285a221fbe02cf
JIHYEMIN <hyemeeeny@gmail.com>
2025-02-14 18:19:29 +0900

    [Style] 자잘한 스타일 수정

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -8,10 +8,10 @@ type DropdownProps = {
 };
 
 const Dropdown = ({ items, isOpen, onClose, onItemClick }: DropdownProps) => {
-  if (!isOpen) return null; // 드롭다운이 닫혀 있으면 렌더링 안 함
+  if (!isOpen) return null;
 
   return (
-    <div className="absolute top-0 right-0 mt-6 rounded-xl shadow-custom bg-white overflow-hidden">
+    <div className="absolute top-0 right-0 mt-10 rounded-xl shadow-custom bg-white overflow-hidden">
       {items.map((item, index) => (
         <DropdownItem
           key={index}
--- src/components/Home/SubSection.tsx
+++ src/components/Home/SubSection.tsx
@@ -1,6 +1,4 @@
-import CtaButton from "@/components/Button/CtaButton";
 import Image from "next/image";
-import Link from "next/link";
 
 const SubSection = () => {
   return (
@@ -11,9 +9,17 @@ const SubSection = () => {
             <span className="gradient-sub-text1">원하는 링크</span>를 <br className="hidden md:block" />
             저장하세요
           </h3>
-          <p className="text-gray07 text-sm">나중에 읽고 싶은 글, 다시 보고 싶은 영상, 사고 싶은 옷, 기억하고 싶은 모든 것을 한 공간에 저장하세요.</p>
+          <p className="text-gray07 text-sm">
+            나중에 읽고 싶은 글, 다시 보고 싶은 영상, 사고 싶은 옷, 기억하고 싶은 모든 것을 한 공간에 저장하세요.
+          </p>
         </div>
-        <Image className="h-auto w-full md:w-[385px] lg:w-[550px]" src="/images/sub_img1.svg" width={550} height={0} alt="sub image1" />
+        <Image
+          className="h-auto w-full md:w-[385px] lg:w-[550px]"
+          src="/images/sub_img1.svg"
+          width={550}
+          height={0}
+          alt="sub image1"
+        />
       </li>
       <li className="flex flex-col md:flex-row-reverse md:items-center md:gap-[51px] lg:gap-[157px]">
         <div>
@@ -24,7 +30,13 @@ const SubSection = () => {
           </h3>
           <p className="text-gray07 text-sm">나만의 폴더를 무제한으로 만들고 다양하게 활용할 수 있습니다.</p>
         </div>
-        <Image className="h-auto w-full md:w-[385px] lg:w-[550px]" src="/images/sub_img2.svg" width={550} height={0} alt="sub image1" />
+        <Image
+          className="h-auto w-full md:w-[385px] lg:w-[550px]"
+          src="/images/sub_img2.svg"
+          width={550}
+          height={0}
+          alt="sub image1"
+        />
       </li>
       <li className="flex flex-col md:flex-row md:items-center md:gap-[51px] lg:gap-[157px]">
         <div>
@@ -33,9 +45,17 @@ const SubSection = () => {
             <br className="hidden md:block" />
             <span className="gradient-sub-text3"> 공유</span>해 보세요
           </h3>
-          <p className="text-gray07 text-sm">여러 링크를 폴더에 담고 공유할 수 있습니다. 가족, 친구, 동료들에게 쉽고 빠르게 링크를 공유해 보세요.</p>
+          <p className="text-gray07 text-sm">
+            여러 링크를 폴더에 담고 공유할 수 있습니다. 가족, 친구, 동료들에게 쉽고 빠르게 링크를 공유해 보세요.
+          </p>
         </div>
-        <Image className="h-auto w-full md:w-[385px] lg:w-[550px]" src="/images/sub_img3.svg" width={550} height={0} alt="sub image1" />
+        <Image
+          className="h-auto w-full md:w-[385px] lg:w-[550px]"
+          src="/images/sub_img3.svg"
+          width={550}
+          height={0}
+          alt="sub image1"
+        />
       </li>
       <li className="flex flex-col md:flex-row-reverse md:items-center md:gap-[51px] lg:gap-[157px]">
         <div>
@@ -46,7 +66,13 @@ const SubSection = () => {
           </h3>
           <p className="text-gray07 text-sm">중요한 정보들을 검색으로 쉽게 찾아보세요.</p>
         </div>
-        <Image className="h-auto w-full md:w-[385px] lg:w-[550px]" src="/images/sub_img4.svg" width={550} height={0} alt="sub image1" />
+        <Image
+          className="h-auto w-full md:w-[385px] lg:w-[550px]"
+          src="/images/sub_img4.svg"
+          width={550}
+          height={0}
+          alt="sub image1"
+        />
       </li>
     </ul>
   );
