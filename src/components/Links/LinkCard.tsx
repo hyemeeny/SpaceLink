7c250a6d343008e933beda5780cfd646b15defc3
JIHYEMIN <hyemeeeny@gmail.com>
2025-02-14 21:09:26 +0900

    [fix] 하이드레이션 오류 함수 추가

--- src/components/Links/LinkCard.tsx
+++ src/components/Links/LinkCard.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { LinkType } from "@/types/links";
 import { GoKebabHorizontal } from "react-icons/go";
 import { formatDate, formatRelativeTime } from "@/utils/dateFormat";
@@ -10,8 +10,21 @@ import UpdateModal from "@/components/Modal/UpdateModal";
 import DeleteModal from "@/components/Modal/DeleteModal";
 import useOnClickOutside from "@/hooks/useOnClickOutside";
 
+// 하이드레이션(hydration) 오류
+export const RelativeTimeComponent = ({ timestamp }: { timestamp: string }) => {
+  const [relativeTime, setRelativeTime] = useState<string>("");
+
+  useEffect(() => {
+    // 클라이언트에서만 시간 계산
+    const relative = formatRelativeTime(timestamp);
+    setRelativeTime(relative);
+  }, [timestamp]);
+
+  return <span>{relativeTime}</span>;
+};
+
 const LinkCard = ({ link }: { link: LinkType }) => {
-  const { openModals, openModal, closeModal } = useModalStore();
+  const { openModals, openModal } = useModalStore();
   const [isOpen, setIsOpen] = useState(false);
   const [selectedLink, setSelectedLink] = useState<LinkType | null>(null);
   const dropdownRef = useRef<HTMLLIElement>(null);
@@ -50,7 +63,7 @@ const LinkCard = ({ link }: { link: LinkType }) => {
       </Link>
       <div className="relative p-4 flex flex-col gap-[10px]">
         <div className="flex justify-between">
-          <p className="text-gray07 text-sm">{formatRelativeTime(link.createdAt)}</p>
+          <p className="text-gray07 text-sm">{RelativeTimeComponent({ timestamp: link.createdAt })}</p>
           <button onClick={toggleDropdown} className="relative w-[21px] h-[17px]">
             <GoKebabHorizontal />
           </button>
