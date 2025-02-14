7cce2c272342595c609992604346214ff162027c
JIHYEMIN <hyemeeeny@gmail.com>
2025-02-14 18:16:24 +0900

    [Chore] 링크 스키마, 토스트 메시지 추가

--- src/lib/toastMessage.ts
+++ src/lib/toastMessage.ts
@@ -2,15 +2,17 @@ const toastMessages = {
   success: {
     login: "로그인에 성공하였습니다.",
     signUp: "회원가입에 성공하였습니다.",
+    addLink: "링크가 추가되었습니다.",
+    updateLink: "링크 URL이 변경되었습니다.",
+    deleteLink: "링크가 삭제되었습니다.",
     addFolder: "폴더가 추가되었습니다.",
     updateFolder: "폴더 이름이 변경되었습니다.",
     deleteFolder: "폴더가 삭제되었습니다.",
-    updateLink: "링크 URL이 변경되었습니다.",
-    deleteLink: "링크가 삭제되었습니다.",
   },
   error: {
     login: "로그인에 실패하였습니다.",
     signUp: "회원가입에 실패하였습니다.",
+    addLink: "링크 URL을 입력해주세요.",
     addFolder: "폴더 이름을 입력해주세요.",
   },
 };
