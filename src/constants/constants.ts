d274ff5af0efa9308b130730c38dd2909d521d46
JIHYEMIN <hyemeeeny@gmail.com>
2025-02-24 22:12:20 +0900

    [Chore] 폴더 및 자잘한 수정

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -2,6 +2,6 @@
 export const ALL_FOLDERS_ID = 0;
 
 // home
-export const count = 10;
-export const MAX_STAR_COUNT = 50;
+export const count = 5;
+export const MAX_STAR_COUNT = 10;
 export const colors = ["#c77eff", "#f6ff7e", "#ff8d7e", "#ffffff"];
