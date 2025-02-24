8d4331f8a7d7be7c6242290af8fa37ea1592aa31
JIHYEMIN <hyemeeeny@gmail.com>
2025-02-24 22:17:18 +0900

    [Refactor] 코드 최적화

--- src/components/Folders/FolderButtonList.tsx
+++ src/components/Folders/FolderButtonList.tsx
@@ -2,40 +2,28 @@ import Image from "next/image";
 import { useFolderStore } from "@/store/useFolderStore";
 import { useModalStore } from "@/store/useModalStore";
 import { FolderButtonListProps } from "@/types/folders";
+import { FaShare, FaPencilAlt, FaTrashAlt } from "react-icons/fa";
 
 const FolderButtonList = ({ handleEditClick, handleDeleteClick, selectedFolder }: FolderButtonListProps) => {
   const { openModal } = useModalStore();
   const { folderId } = useFolderStore();
 
+  const buttons = [
+    { id: 1, icon: <FaShare />, label: "공유", onClick: () => openModal(`shareFolder-${folderId}`) },
+    { id: 2, icon: <FaPencilAlt />, label: "이름 변경", onClick: () => handleEditClick(selectedFolder!) },
+    { id: 3, icon: <FaTrashAlt />, label: "삭제", onClick: () => handleDeleteClick(selectedFolder!) },
+  ];
+
   return (
     <>
       {folderId !== 0 && (
         <div className="flex gap-3">
-          <button
-            onClick={() => openModal(`shareFolder-${folderId}`)}
-            className="flex items-center gap-1 text-gray04 text-sm font-medium"
-          >
-            <Image src={"/icons/share.svg"} width={18} height={18} alt="공유" />
-            공유
-          </button>
-
-          {/* 폴더 이름 변경 */}
-          <button
-            onClick={() => handleEditClick(selectedFolder!)}
-            className="flex items-center gap-1 text-gray04 text-sm font-medium"
-          >
-            <Image src={"/icons/pen.svg"} width={18} height={18} alt="이름 변경" />
-            이름 변경
-          </button>
-
-          {/* 폴더 삭제 */}
-          <button
-            onClick={() => handleDeleteClick(selectedFolder!)}
-            className="flex items-center gap-1 text-gray04 text-sm font-medium"
-          >
-            <Image src={"/icons/delete.svg"} width={18} height={18} alt="삭제" />
-            삭제
-          </button>
+          {buttons.map((button) => (
+            <button key={button.id} onClick={button.onClick} className="flex items-center gap-1 text-sm font-medium">
+              {button.icon}
+              {button.label}
+            </button>
+          ))}
         </div>
       )}
     </>
