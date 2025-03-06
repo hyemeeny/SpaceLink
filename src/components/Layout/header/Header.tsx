2f9a06f26a1eb52dd6f6d750507bebad2b13a3b9
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-06 20:40:28 +0900

    [Chore] 로고 및 메인 수정

--- src/components/Layout/header/Header.tsx
+++ src/components/Layout/header/Header.tsx
@@ -1,6 +1,5 @@
 import { cookies } from "next/headers";
 import Link from "next/link";
-import Image from "next/image";
 import API_URL from "@/constants/config";
 import User from "@/components/Layout/header/User";
 import CtaButton from "@/components/Button/CtaButton";
@@ -37,33 +36,10 @@ const getUser = async () => {
 const Header = async () => {
   const user = await getUser();
 
-  // 사용자 데이터를 로딩 중일 때 빈 상태로 유지
-  // if (isPending) {
-  //   return (
-  //     <header className="bg-gray03 flex justify-between items-center px-5 py-3">
-  //       <div className="w-[133px] h-[24px] bg-gray-200 animate-pulse"></div>
-  //       <div className="w-[80px] h-[37px] bg-gray-200 animate-pulse"></div>
-  //     </header>
-  //   );
-  // }
-
-  // 사용자 데이터를 불러오는 데 실패했을 때
-  // if (isError) {
-  //   return (
-  //     <header className="bg-gray03 flex justify-between items-center px-5 py-3">
-  //       <Link href={"/"}>
-  //         <Image src={"/icons/logo.svg"} width={133} height={24} alt="Linkbrary" />
-  //       </Link>
-  //       <p className="text-red-500">유저 정보를 가져오지 못했습니다.</p>
-  //     </header>
-  //   );
-  // }
-
   return (
     <header className="relative bg-transparent flex justify-between items-center md:w-[768px] lg:w-[1024px] xl:w-[1280px] mx-auto p-4 md:py-8 z-10">
       <Link href={"/"}>
-        {/* <Image src={"/icons/logo.svg"} width={133} height={24} alt="Linkbrary" /> */}
-        <h1 className="font-pyeongChangPeace text-3xl font-bold text-purple01">우주링크</h1>
+        <h1 className="font-pyeongChangPeace text-3xl font-bold text-purple01">SpaceLink</h1>
       </Link>
       {user ? (
         <User user={user} />
