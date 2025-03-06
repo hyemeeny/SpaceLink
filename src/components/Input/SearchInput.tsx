ff23182c612951f0d6c578503bcd52da01cd23c3
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-06 20:39:04 +0900

    [Refactor] 검색 기능 수정

--- src/components/Input/SearchInput.tsx
+++ src/components/Input/SearchInput.tsx
@@ -1,28 +1,62 @@
 "use client";
 
-import Image from "next/image";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { ChangeEventHandler } from "react";
+import { useEffect, useState } from "react";
+import useCustomSearchParams from "@/hooks/useCustomSearchParams";
+import { RiSearchLine } from "react-icons/ri";
+import { TiDelete } from "react-icons/ti";
 
-interface SearchInputProps {
-  search: string;
-  handleSearchChange: ChangeEventHandler;
-}
+const SearchInput = () => {
+  const { searchParams, setSearchParams } = useCustomSearchParams();
+  const [search, setSearch] = useState(searchParams.search || "");
 
-const SearchInput = ({ search, handleSearchChange }: SearchInputProps) => {
+  const handleSearch = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setSearchParams({ search });
+  };
+
+  const handleReset = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    setSearch("");
+    setSearchParams({ search: "" });
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      setSearchParams({ search });
+    }
+  };
+
+  useEffect(() => {
+    setSearch(searchParams.search);
+  }, [searchParams.search]);
+
   return (
-    <div className="flex items-center justify-end relative h-[43px] md:h-[54px] md:mb-4">
-      <input
-        type="text"
-        name="search"
-        value={search}
-        onChange={handleSearchChange}
-        placeholder="링크를 검색해 보세요"
-        className="w-full h-full bg-white02 rounded-xl text-sm md:text-base font-medium placeholder-#666 text-gray06 pl-10"
-      />
-      <button type="button" className="absolute left-4">
-        <Image src={"/icons/search.svg"} width={16} height={16} alt="검색" />
-      </button>
+    <div className="flex flex-col gap-6 mt-4 mb-8">
+      <div className="flex items-center justify-end relative h-[43px] md:h-[54px]">
+        <input
+          type="text"
+          name="search"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={handleKeyDown}
+          placeholder="링크를 검색해 보세요"
+          className="w-full h-full bg-white02 rounded-xl text-sm md:text-base font-medium placeholder-gray04 text-gray06 pl-11"
+        />
+        <button type="button" className="absolute left-4" onClick={handleSearch}>
+          <RiSearchLine className="w-5 h-5 text-gray04" />
+        </button>
+        {search && (
+          <button type="button" className="absolute right-4" onClick={handleReset}>
+            <TiDelete className="w-7 h-7 text-gray03" />
+          </button>
+        )}
+      </div>
+      {search && (
+        <h4 className="text-3xl text-gray03">
+          <span className="text-white font-semibold">{search}</span>으로 검색한 결과입니다.
+        </h4>
+      )}
     </div>
   );
 };
