695d68a3fdf145aebcdcc6bd37bac4022ef8092c
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-12 20:12:08 +0900

    [Refactor] 드롭다운 간소화

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,11 +1,5 @@
-interface DropdownItemType {
-  label: string;
-  value?: string;
-  icon?: React.ReactNode;
-}
-
 interface DropdownProps {
-  items: DropdownItemType[];
+  items: string[];
   isOpen: boolean;
   onClose: () => void;
   onItemClick: (value: string) => void;
@@ -16,18 +10,17 @@ const Dropdown = ({ items, isOpen, onClose, onItemClick }: DropdownProps) => {
 
   return (
     <div className="absolute top-0 right-0 mt-10 p-2 rounded-lg shadow-custom bg-white overflow-hidden">
-      {items.map((item, index) => (
-        <div
-          key={index}
+      {items.map((item) => (
+        <button
+          key={item}
           onClick={() => {
-            if (item.value) onItemClick(item.value);
+            onItemClick(item);
             onClose();
           }}
           className="flex items-center gap-2 w-max text-sm text-gray06 cursor-pointer px-4 py-2 rounded-md hover:bg-gray01  transition duration-500 ease-in-out"
         >
-          {item.icon && <span>{item.icon}</span>}
-          <span>{item.label}</span>
-        </div>
+          {item}
+        </button>
       ))}
     </div>
   );
