969a9439dae02a5169262d2101d672cf53cefae7
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-12 20:11:42 +0900

    [Chore] 메인 컨테이너 반응형 수정

--- src/components/Home/ContainerSection.tsx
+++ src/components/Home/ContainerSection.tsx
@@ -5,8 +5,8 @@ import Container from "@/components/Layout/Container";
 const ContainerSection = ({ children, lottie }: { children: ReactNode; lottie: object }) => {
   return (
     <Container className="flex flex-col lg:flex-row lg:items-center justify-center lg:justify-between gap-y-8 lg:gap-y-0 h-[calc(100vh-101px)]">
-      <div className="flex flex-col gap-y-5">{children}</div>
-      <Lottie animationData={lottie} className="w-[300px] md:w-[500px] lg:w-[600px]" />
+      <div className="flex flex-col gap-y-5 text-center lg:text-left">{children}</div>
+      <Lottie animationData={lottie} className="w-[300px] md:w-[500px] lg:w-[600px] mx-auto lg:mx-0" />
     </Container>
   );
 };
