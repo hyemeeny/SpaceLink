0670460b1bb4379ec1ef2ba9a9c64dce6c919507
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-17 22:56:19 +0900

    [Chore] 메인 페이지 문구 수정

--- src/components/Home/SubSection04.tsx
+++ src/components/Home/SubSection04.tsx
@@ -9,8 +9,8 @@ const SubSection04 = () => {
         <br className="hidden md:block" />
         <span className="gradient-text"> 공유</span>해 보세요
       </h3>
-      <p className="text-gray01 text-sm">
-        여러 링크를 폴더에 담고 공유할 수 있습니다.
+      <p className="text-gray01 text-sm md:text-base">
+        여러 링크를 폴더에 담고 공유할 수 있어요.
         <br /> 가족, 친구, 동료들에게 쉽고 빠르게 링크를 공유해 보세요.
       </p>
     </ContainerSection>
