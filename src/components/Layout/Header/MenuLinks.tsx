d6a46dff81b8337862d4743e75ffd1a125bdbbe3
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-17 20:46:57 +0900

    [Refactor] 모바일 메뉴 수정

---
+++ src/components/Layout/Header/MenuLinks.tsx
@@ -0,0 +1,42 @@
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter, usePathname } from "next/navigation";
+import { Dispatch, SetStateAction } from "react";
+import { logout } from "@/actions/auth";
+import { FiLink, FiStar, FiLogOut } from "react-icons/fi";
+
+interface MenuLinksProps {
+  setIsOpen?: Dispatch<SetStateAction<boolean>>;
+}
+
+const MenuLinks = ({ setIsOpen }: MenuLinksProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleLogout = async () => {
+    await logout();
+    router.push("/login");
+    setIsOpen?.(false);
+  };
+
+  const navLinkBase =
+    "flex items-center gap-3 w-full text-base cursor-pointer px-4 py-3 rounded-md hover:bg-purple01 transition duration-300 ease-in-out";
+  const activeStyle = "bg-purple01";
+
+  return (
+    <div className="flex flex-col gap-2">
+      <Link href="/links" className={clsx(navLinkBase, pathname === "/links" ? activeStyle : "")}>
+        <FiLink className="size-5 md:size-6" /> 링크 보관소
+      </Link>
+      <Link href="/favorite" className={clsx(navLinkBase, pathname === "/favorite" ? activeStyle : "")}>
+        <FiStar className="size-5 md:size-6" /> 즐겨찾기
+      </Link>
+      <hr className="border-purple02 mx-2" />
+      <button onClick={handleLogout} className={navLinkBase}>
+        <FiLogOut className="size-5 md:size-6" /> 로그아웃
+      </button>
+    </div>
+  );
+};
+
+export default MenuLinks;
--- src/components/Layout/Header/UserMenu.tsx
+++ src/components/Layout/Header/UserMenu.tsx
@@ -1,33 +1,19 @@
 "use client";
 
 import { useRef, useState } from "react";
-import { usePathname, useRouter } from "next/navigation";
 import { UserProps } from "@/types/auth";
-import { logout } from "@/actions/auth";
 import clsx from "clsx";
-import Link from "next/link";
 import Image from "next/image";
 import useOnClickOutside from "@/hooks/useOnClickOutside";
-import { FiLink, FiStar, FiLogOut } from "react-icons/fi";
+import MenuLinks from "@/components/Layout/Header/MenuLinks";
 
 const UserMenu = ({ user }: { user: UserProps | null }) => {
-  const router = useRouter();
-  const pathname = usePathname();
   const [isOpen, setIsOpen] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   useOnClickOutside(dropdownRef, () => setIsOpen(false));
 
-  const handleLogout = async () => {
-    await logout();
-    router.push("/login");
-    setIsOpen(false);
-  };
-
   const menuIcon = "bg-white w-6 h-[2px] rounded transition-all duration-300";
-  const navLinkBase =
-    "flex items-center gap-3 w-full text-base cursor-pointer px-4 py-3 rounded-md hover:bg-purple01 transition duration-300 ease-in-out";
-  const activeStyle = "bg-purple01";
 
   return (
     <div className="flex items-center gap-2" ref={dropdownRef}>
@@ -37,7 +23,7 @@ const UserMenu = ({ user }: { user: UserProps | null }) => {
           <p className="text-xl">{user.name}</p>
 
           <button
-            className="flex flex-col justify-center items-center w-10 h-10 relative"
+            className="flex flex-col justify-center items-center w-10 h-10 relative z-50"
             onClick={() => setIsOpen(!isOpen)}
           >
             <div className={clsx(menuIcon, "absolute", isOpen ? "rotate-45 top-1/2" : "-translate-y-2")} />
@@ -45,19 +31,34 @@ const UserMenu = ({ user }: { user: UserProps | null }) => {
             <div className={clsx(menuIcon, "absolute", isOpen ? "-rotate-45 top-1/2" : "translate-y-2")} />
           </button>
 
+          {/* 데스크탑 */}
           {isOpen && (
-            <nav className="flex flex-col gap-2 w-60 absolute top-2 right-0 mt-10 p-2 rounded-xl shadow-custom bg-black02 bg-opacity-50 overflow-hidden">
-              <Link href="/links" className={clsx(navLinkBase, pathname === "/links" ? activeStyle : "")}>
-                <FiLink className="size-5" /> 링크 보관소
-              </Link>
-              <Link href="/favorite" className={clsx(navLinkBase, pathname === "/favorite" ? activeStyle : "")}>
-                <FiStar className="size-5" /> 즐겨찾기
-              </Link>
-              <hr className="border-purple02 mx-2" />
-              <button onClick={handleLogout} className={clsx(navLinkBase)}>
-                <FiLogOut className="size-5" /> 로그아웃
-              </button>
+            <nav className="hidden md:flex flex-col gap-2 w-60 absolute top-2 right-0 mt-10 p-2 rounded-xl shadow-custom bg-black02 bg-opacity-70 overflow-hidden">
+              <MenuLinks />
             </nav>
+          )}
+
+          {/* 모바일 */}
+          {isOpen && (
+            <>
+              <div
+                className={clsx(
+                  "fixed md:hidden top-0 right-0 w-full h-full bg-black bg-opacity-50 transition-opacity duration-300",
+                  isOpen ? "opacity-100 visible" : "opacity-0 invisible",
+                )}
+              />
+
+              <nav
+                className={clsx(
+                  "fixed md:hidden top-0 right-0 h-full w-3/4 max-w-xs bg-black02 shadow-lg transform transition-transform duration-300",
+                  isOpen ? "translate-x-0" : "translate-x-full",
+                )}
+              >
+                <div className="pt-16 px-2">
+                  <MenuLinks setIsOpen={setIsOpen} />
+                </div>
+              </nav>
+            </>
           )}
         </div>
       )}
