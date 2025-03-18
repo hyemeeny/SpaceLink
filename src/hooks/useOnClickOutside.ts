522f751421d0d51ca092e236b69bfac14eef6ea2
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-19 00:04:09 +0900

    [Fix] 빌드 오류 로티 테스트

--- src/hooks/useOnClickOutside.ts
+++ src/hooks/useOnClickOutside.ts
@@ -2,8 +2,6 @@ import { RefObject, useEffect } from "react";
 
 const useOnClickOutside = (ref: RefObject<HTMLElement>, handler: Function) => {
   useEffect(() => {
-    if (typeof window === "undefined") return;
-
     const handleClickOutside = (event: MouseEvent) => {
       if (ref.current && !ref.current.contains(event.target as Node)) {
         handler(event);
