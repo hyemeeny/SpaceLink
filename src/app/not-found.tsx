e308b07e17c8c647ed526e6d0d39db1db8bdb642
JIHYEMIN <hyemeeeny@gmail.com>
2025-03-18 21:57:58 +0900

    [Refactor] notfound 페이지 수정

---
+++ src/app/not-found.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Lottie from "lottie-react";
+import notfound from "@/assets/lotties/notfound.json";
+import Link from "next/link";
+import CtaButton from "@/components/Button/CtaButton";
+
+const Notfound = () => {
+  return (
+    <section className="flex flex-col items-center justify-center mt-32">
+      <h2 className="text-3xl md:text-4xl font-semibold mb-2">404 ERROR</h2>
+      <p className="text-gray03 text-sm md:text-base">해당 페이지를 찾을 수 없어요!</p>
+      <Lottie animationData={notfound} className="w-[200px] md:w-[300px]" />
+      <Link href={"/"}>
+        <CtaButton>메인으로 돌아가기</CtaButton>
+      </Link>
+    </section>
+  );
+};
+
+export default Notfound;
