62a191b89b433a7437ccd8b566d2cce96789ed02
JIHYEMIN <hyemeeeny@gmail.com>
2025-04-01 21:54:25 +0900

    [Fix] 모달 오류 3차 테스트

--- src/components/Modal/ModalContainer.tsx
+++ src/components/Modal/ModalContainer.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React from "react";
 import Modal from "react-modal";
 import Image from "next/image";
 import { ReactNode } from "react";
@@ -24,6 +27,8 @@ const ModalContainer = ({ children, modalId, ...props }: ModalContainerProps) =>
       contentLabel="Modal Container"
       className="flex flex-col items-center fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 pb-8 w-[90%] md:w-[360px] rounded-2xl bg-white"
       overlayClassName="fixed inset-0 bg-black bg-opacity-50 z-50"
+      appElement={document.getElementById("modal-root") || undefined}
+      ariaHideApp={false}
       {...props}
     >
       <button onClick={() => closeModal(modalId)} className="ml-auto">
