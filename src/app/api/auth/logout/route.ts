bcb00efcbd7d994734df219916b631baa93a22bb
지혜민 <hyemeeeny@gmail.com>
2026-06-02 21:18:13 +0900

    [Refactor] user, login, logout api 라우트 핸들러로 변경

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,13 @@
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const POST = async () => {
+  try {
+    cookies().delete("accessToken");
+
+    return NextResponse.json({ message: "로그아웃 성공" }, { status: 200 });
+  } catch (error) {
+    console.error("로그아웃 중 오류 발생", error);
+    return NextResponse.json({ message: "로그아웃 실패" }, { status: 500 });
+  }
+};
