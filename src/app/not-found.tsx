6d715500647c2d095f9da7aad63f52d59e5b2bb1
지혜민 <hyemeeeny@gmail.com>
2026-06-17 19:20:33 +0900

    [Style] 404 페이지 레이아웃 헤더 높이 고려해 중앙 정렬로 수정

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,14 +3,17 @@
 import notfound from "@/assets/lotties/notfound.json";
 import CtaButton from "@/components/Button/CtaButton";
 import dynamic from "next/dynamic";
-const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+const Lottie = dynamic(() => import("lottie-react"), {
+  ssr: false,
+  loading: () => <div className="w-[200px] md:w-[400px] h-[200px] md:h-[400px]" />,
+});
 
 const Notfound = () => {
   return (
-    <section className="flex flex-col items-center justify-center mt-32">
+    <section className="flex flex-col items-center justify-center min-h-[calc(100vh-72px)] md:min-h-[calc(100vh-124px)]">
       <h2 className="text-3xl md:text-4xl font-semibold mb-2">404 ERROR</h2>
       <p className="text-gray03 text-sm md:text-base">해당 페이지를 찾을 수 없어요!</p>
-      <Lottie animationData={notfound} className="w-[200px] md:w-[300px]" />
+      <Lottie animationData={notfound} className="w-[200px] md:w-[400px] h-[200px] md:h-[400px]" />
       <CtaButton url="/">메인으로 돌아가기</CtaButton>
     </section>
   );
