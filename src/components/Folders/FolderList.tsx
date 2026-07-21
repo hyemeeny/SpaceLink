d2b867bccdffe3c90447967bf2a0f3260b8375d8
지혜민 <hyemeeeny@gmail.com>
2026-07-21 19:55:29 +0900

    [Feat] 모바일에서 스크롤 가운데 정렬 추가

--- src/components/Folders/FolderList.tsx
+++ src/components/Folders/FolderList.tsx
@@ -1,35 +1,54 @@
-import { Folder } from "@/types/folder";
+import { useEffect, useRef } from "react";
+import { useFolders } from "@/hooks/queries/useFolders";
 import FolderButton from "@/components/Button/FolderButton";
 
 interface FolderListProps {
-  folders: Folder[];
   folderId: number | null;
   onSelect: (folderId: number | null) => void;
 }
 
-const FolderList = ({ folders, folderId, onSelect }: FolderListProps) => {
-  return (
-    <div role="tablist" className="flex flex-wrap mr-auto gap-2">
-      <FolderButton
-        onClick={() => onSelect(null)}
-        role="tab"
-        aria-selected={folderId === null}
-        isSelected={folderId === null}
-      >
-        전체
-      </FolderButton>
+const FolderList = ({ folderId, onSelect }: FolderListProps) => {
+  const { data: folders = [] } = useFolders();
+  const tabRefs = useRef<Record<string | number, HTMLButtonElement | null>>({});
+  const allFolders = [{ id: null, name: "전체" }, ...folders];
 
-      {folders.map((folder) => (
-        <FolderButton
-          key={folder.id}
-          role="tab"
-          aria-selected={folderId === folder.id}
-          isSelected={folderId === folder.id}
-          onClick={() => onSelect(folder.id)}
-        >
-          {folder.name}
-        </FolderButton>
-      ))}
+  useEffect(() => {
+    const key = folderId ?? "all";
+    const el = tabRefs.current[key];
+
+    if (el) {
+      el.scrollIntoView({
+        behavior: "smooth",
+        inline: "center",
+        block: "nearest",
+      });
+    }
+  }, [folderId]);
+
+  return (
+    <div className="w-full overflow-hidden folder-scroll">
+      <div role="tablist" className="flex flex-nowrap overflow-x-auto gap-2">
+        {allFolders.map((folder) => {
+          const key = folder.id ?? "all";
+          const isSelected = folderId === folder.id;
+
+          return (
+            <FolderButton
+              key={key}
+              ref={(el) => {
+                tabRefs.current[key] = el;
+              }}
+              role="tab"
+              aria-selected={isSelected}
+              isSelected={isSelected}
+              onClick={() => onSelect(folder.id)}
+              className="whitespace-nowrap"
+            >
+              {folder.name}
+            </FolderButton>
+          );
+        })}
+      </div>
     </div>
   );
 };
